067cf1a5349ee1036a3b933d2100e3d87f1d820a
nberger <nicoberger@gmail.com>
2012-05-04 01:58:24 -0300

    Update ruby with my current config

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,8 @@
-alias r='rbenv local 1.8.7-p358'
+alias b="bundle exec"
+alias br='bundle exec rails'
 
+alias bkm='b rake db:migrate db:test:prepare'
+
+alias scs='script/server --debugger'
 alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
+alias scd='script/console --debugger'

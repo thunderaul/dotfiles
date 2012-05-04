9d0f9df8234ffaf3377b6a614af719e5ef9f8e72
nberger <nicoberger@gmail.com>
2012-05-04 01:58:49 -0300

    Update system with my current config

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,1 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="./bin:$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
